2f36d228b579af62650504566d27c373f7e6234a
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-02-21 11:43:14 +0000

    loan read out

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principle: ");
+        int principle = scanner.nextInt();
+        System.out.println("Your principle is " + principle);
     }
 }
